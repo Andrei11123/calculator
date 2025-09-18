a9b06970b115dbce0ed40ca314f1d22627743b54
Andrei <andrei.julavschi@mail.ru>
2025-09-18 22:37:32 +0300

    Am adaugat fundal negru si display numar

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,19 +1,47 @@
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Arrays;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 
 public class Calculator {
-    int boardwidth = 400;
-    int boardheight = 600;
+    int boardWidth = 360;
+    int boardHeight = 540;
 
-    JFrame frame = new JFrame("Calculator"); /*constructor */
+    Color customLightGray = new Color(212, 212, 210);
+    Color customDarkGray = new Color(80, 80, 80);
+    Color customBlack = new Color(28, 28, 28);
+    Color customOrange = new Color(255, 149, 0);
 
-    Calculator () {
-        frame.setVisible(true);
-        frame.setSize(boardwidth, boardheight);
+
+    JFrame frame = new JFrame("Calculator");
+    JLabel displayLabel = new JLabel();
+    JPanel displayPanel = new JPanel();
+    JPanel buttonsPanel = new JPanel();
+
+
+    public Calculator() {
+        frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        displayLabel.setBackground(customBlack);
+        displayLabel.setForeground(Color.white);
+        displayLabel.setFont(new Font("Arial", Font.PLAIN, 80));
+        displayLabel.setHorizontalAlignment(JLabel.RIGHT);
+        displayLabel.setText("0");
+        displayLabel.setOpaque(true);
+
+        displayPanel.setLayout(new BorderLayout());
+        displayPanel.add(displayLabel);
+        frame.add(displayPanel, BorderLayout.NORTH);
+
+        buttonsPanel.setLayout(new GridLayout(5, 4));
+        buttonsPanel.setBackground(customBlack);
+        frame.add(buttonsPanel, BorderLayout.CENTER);
+
+
+        frame.setVisible(true);
     }
 }
-
