693ca1e994de7b7c1284ce71af1fc8e823e5f66b
Andrei <andrei.julavschi@mail.ru>
2025-09-18 18:59:02 +0300

    Fereastra la calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,6 +5,15 @@ import javax.swing.*;
 import javax.swing.border.LineBorder;
 
 public class Calculator {
+    int boardwidth = 400;
+    int boardheight = 600;
 
+    JFrame frame = new JFrame("Calculator"); /*constructor */
+
+    Calculator () {
+        frame.setVisible(true);
+        frame.setSize(boardwidth, boardheight);
+        frame.setLocationRelativeTo(null);
+    }
 }
 
